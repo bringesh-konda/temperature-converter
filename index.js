62ea23985ee053b7def1b1d0a38db427675b7432
bringesh-konda <bringesh03@gmail.com>
2023-09-13 17:14:05 +0530

    after modifying js with setting listener as input

--- index.js
+++ index.js
@@ -2,33 +2,24 @@ function roundToTwo(num) {
     return +(Math.round(num + "e+2")  + "e-2");
 }
 
-var form = document.querySelector(".converter");
+var celcius = document.getElementById("celcius");
+var fahrenheit = document.getElementById("fahrenheit");
+var kelvin = document.getElementById("kelvin");
 
-form.addEventListener("submit", (event)=>{
-    event.preventDefault();
+celcius.addEventListener("input", ()=>{
+    var inp = parseFloat(celcius.value);
+    fahrenheit.value = roundToTwo((1.8 * inp) + 32);
+    kelvin.value = roundToTwo(inp + 273.15);
+});
 
-    var inp1 = document.querySelector("#input1").value;
-    var inp2 = document.querySelector("#input2").value;
-    var inp3 = document.querySelector("#input3").value;
+fahrenheit.addEventListener("input", ()=>{
+    var inp = parseFloat(fahrenheit.value);
+    celcius.value = roundToTwo((inp - 32) / 1.8);
+    kelvin.value = roundToTwo(((inp - 32)  / 1.8) + 273.15);
+});
 
-    inp3 = parseFloat(inp3);
-    console.log(inp1, inp2, inp3);
-    var ans = inp3;
-
-    if(inp1 == "Fahrenheit" && inp2 == "Celcius")
-        ans = (inp3 - 32) / 1.8;
-    else if(inp1 == "Fahrenheit" && inp2 == "Kelvin")
-        ans = ((inp3 - 32) / 1.8) + 273.15;
-    else if(inp1 == "Celcius" && inp2 == "Fahrenheit")
-        ans = (1.8 * inp3) + 32;
-    else if(inp1 == "Celcius" && inp2 == "Kelvin")
-        ans = inp3 + 273.15;
-    else if(inp1 == "Kelvin" && inp2 == "Celcius")
-        ans = inp3 - 273.15;
-    else if(inp1 == "Kelvin" && inp2 == "Fahrenheit")
-        ans = ((inp3 - 273.15) * 1.8) + 32; 
-
-    ans = roundToTwo(ans);
-    document.querySelector("p").innerHTML = "Answer : " + 
-    ans;
+kelvin.addEventListener("input", ()=>{
+    var inp = parseFloat(kelvin.value);
+    celcius.value = roundToTwo(inp - 273.15);
+    fahrenheit.value = roundToTwo(((inp - 273.15) * 1.8) + 32);
 });
